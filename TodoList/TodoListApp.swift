7b342a05d359f3bff9d9db291aa3982ab834b62c
DivyaKarunakaran <divyaapril10@gmail.com>
2026-07-16 10:12:16 +1200

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by DivyaKarunakaran on 16/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
